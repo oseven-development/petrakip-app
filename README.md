6f8846d3a3efeb31293bdda3100c1dbb936ca377
philschmid <schmidphilipp1995@gmail.com>
2021-04-23 20:22:48 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # petra-kip-app
+
+
---
+++ frontend/src/App.tsx
@@ -0,0 +1,73 @@
+import { Redirect, Route } from 'react-router-dom';
+import {
+  IonApp,
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+} from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { ellipse, square, triangle } from 'ionicons/icons';
+import Moments from './pages/moments';
+import Reflections from './pages/reflections';
+import Profile from './pages/profile';
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
+const App: React.FC = () => (
+  <IonApp>
+    <IonReactRouter>
+      <IonTabs>
+        <IonRouterOutlet>
+          <Route exact path="/moments">
+            <Moments />
+          </Route>
+          <Route exact path="/reflections">
+            <Reflections />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/moments" />
+          </Route>
+        </IonRouterOutlet>
+        <IonTabBar slot="bottom">
+          <IonTabButton tab="moments" href="/moments">
+            <IonIcon icon={triangle} />
+            <IonLabel>Momente</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="reflections" href="/reflections">
+            <IonIcon icon={ellipse} />
+            <IonLabel>Reflexionen</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="profile" href="/profile">
+            <IonIcon icon={square} />
+            <IonLabel>Profil</IonLabel>
+          </IonTabButton>
+        </IonTabBar>
+      </IonTabs>
+    </IonReactRouter>
+  </IonApp>
+);
+
+export default App;
