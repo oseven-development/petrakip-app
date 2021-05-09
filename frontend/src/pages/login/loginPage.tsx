82a64a0612d5cf010c81ea1854b0045ecf23e473
Stefan Blos <stefan.blos@gmail.com>
2021-05-09 21:45:29 +0200

    added a loginPage

---
+++ frontend/src/pages/login/loginPage.tsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react'
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonPage,
+  IonSpinner,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react'
+
+import './loginPage.css'
+import { useHistory } from 'react-router-dom'
+
+const LoginPage: React.FC = () => {
+  const [mail, setMail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [loginLoading, setLoginLoading] = useState(false)
+
+  const history = useHistory()
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Metapholio</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Metapholio</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <div className="login-container">
+          <p>Willkommen bei Metapholio, bitte melden sie sich an:</p>
+          <IonCard className="input-card">
+            <IonCardContent>
+              <IonItem>
+                <IonLabel position="stacked">Mailadresse</IonLabel>
+                <IonInput
+                  autocomplete="email"
+                  inputmode="email"
+                  pattern="email"
+                  placeholder="Mailadresse eingeben"
+                  type="email"
+                  value={mail}
+                  onIonChange={e => setMail(e.detail.value ?? '')}
+                ></IonInput>
+              </IonItem>
+              <IonItem>
+                <IonLabel position="stacked">Passwort</IonLabel>
+                <IonInput
+                  pattern="password"
+                  placeholder="Passwort eingeben"
+                  type="password"
+                  value={password}
+                  onIonChange={e => setPassword(e.detail.value ?? '')}
+                ></IonInput>
+              </IonItem>
+            </IonCardContent>
+          </IonCard>
+          <div className="button-container">
+            <IonButton
+              expand="block"
+              onClick={() => {
+                /**
+                 * TODO: Dummy solution for now
+                 */
+                setLoginLoading(true)
+                console.log(`Trying to login with ${mail} and ${password}`)
+                setTimeout(() => {
+                  history.push('/app/tab1')
+                }, 2000)
+              }}
+            >
+              {loginLoading ? <IonSpinner /> : 'Anmelden'}
+            </IonButton>
+            {/* TODO: set correct href to register page */}
+            <IonButton expand="block" fill="outline" href="/app/tab2">
+              Registrieren
+            </IonButton>
+          </div>
+        </div>
+      </IonContent>
+    </IonPage>
+  )
+}
+
+export default LoginPage
