c301b1cc3dcfc4c430598e4eb3b30f61394c05d3
Maximilian Hänsel <mh@maxhaensel.de>
2021-05-14 14:29:43 +0200

    Reset ListView

--- frontend/src/pages/moments/momentsListView.tsx
+++ frontend/src/pages/moments/momentsListView.tsx
@@ -1,108 +1,9 @@
-import {
-  IonContent,
-  IonPage,
-  IonItem,
-  IonLabel,
-  IonButton,
-  IonListHeader,
-  IonText,
-  IonRouterLink,
-  IonItemDivider,
-} from '@ionic/react'
-import { Header, MomentCard } from '../../components'
+import { IonContent, IonPage, IonButton } from '@ionic/react'
+import { Header } from '../../components'
 import { RouteComponentProps } from 'react-router'
-
-import { API, graphqlOperation } from 'aws-amplify'
-import { listMoments } from '../../graphql/queries'
-
-import {
-  TextVariantWithProps,
-  ImageVariantWithProps,
-  AudioVariantWithProps,
-  VideoVariantWithProps,
-} from '../../components/card/moments/basicCard'
-import React from 'react'
-
 interface Props extends RouteComponentProps<{}> {}
 
-const data: {
-  date: string
-  dataset: (
-    | TextVariantWithProps
-    | ImageVariantWithProps
-    | AudioVariantWithProps
-    | VideoVariantWithProps
-  )[]
-}[] = [
-  {
-    date: '24.04.2021 Mittwoch',
-    dataset: [
-      {
-        momentId: 1,
-        title: 'Text Card',
-        variant: 'text',
-        text: 'asd',
-      },
-      {
-        momentId: 1,
-        title: 'Text Card',
-        variant: 'image',
-        image: 'asd',
-      },
-      {
-        momentId: 1,
-        title: 'Text Card',
-        variant: 'audio',
-        audio: 'asd',
-      },
-      {
-        momentId: 1,
-        title: 'Text Card',
-        variant: 'video',
-        video: 'asd',
-      },
-    ],
-  },
-  {
-    date: '23.04.2021 Dienstag',
-    dataset: [
-      {
-        momentId: 1,
-        title: 'Text Card',
-        variant: 'text',
-        text: 'asd',
-      },
-      {
-        momentId: 1,
-        title: 'Text Card',
-        variant: 'image',
-        image: 'asd',
-      },
-      {
-        momentId: 1,
-        title: 'Text Card',
-        variant: 'audio',
-        audio: 'asd',
-      },
-      {
-        momentId: 1,
-        title: 'Text Card',
-        variant: 'video',
-        video: 'asd',
-      },
-    ],
-  },
-]
-
 export const MomentsListView: React.FC<Props> = ({ history }) => {
-  React.useEffect(() => {
-    const fetch = async () => {
-      const x = await API.graphql(graphqlOperation(listMoments))
-      console.log(x)
-    }
-    fetch()
-  }, [])
-
   return (
     <IonPage>
       <Header>Momente List View</Header>
@@ -111,19 +12,7 @@ export const MomentsListView: React.FC<Props> = ({ history }) => {
           Erstellen
         </IonButton>
         <br></br>
-        {data.map((daily, indexDay) => (
-          <React.Fragment key={`daily-moments-${indexDay}`}>
-            <IonLabel className="ion-padding-start" color="medium">
-              {daily.date}
-            </IonLabel>
-            {daily.dataset.map((cardDataset, indexCard) => (
-              <MomentCard
-                key={`daily-moments-${indexDay}-card-${indexCard}`}
-                {...cardDataset}
-              />
-            ))}
-          </React.Fragment>
-        ))}
+        Moment Content
       </IonContent>
     </IonPage>
   )
