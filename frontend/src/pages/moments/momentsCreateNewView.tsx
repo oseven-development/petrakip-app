c9aa005cc1c1446aa2c2e34d064050b4ffac428c
philschmid <schmidphilipp1995@gmail.com>
2021-05-14 13:12:47 +0200

    imagerecorder

--- frontend/src/pages/moments/momentsCreateNewView.tsx
+++ frontend/src/pages/moments/momentsCreateNewView.tsx
@@ -6,6 +6,7 @@ import { usePlatform } from '../../hooks/usePlatform'
 import { useState } from 'react'
 import { AudioRecorder } from '../../components/media/audioRecorder'
 import { VideoRecorder } from '../../components/media/videoRecorder'
+import { ImageRecorder } from '../../components/media/imageRecorder'
 interface Props
   extends RouteComponentProps<{
     id: string
@@ -26,27 +27,16 @@ export const MomentsCreateNewView: React.FC<Props> = ({ match, history }) => {
       <Header>Momente Erstellen</Header>
       <IonContent fullscreen>
         {platform}
-        <p>
-          Capture Video:
-          <input
-            type="file"
-            accept="video/*"
-            id="capture"
-            capture="camcorder"
-          />
-        </p>
-        <p>
-          Capture Audio: <input type="file" accept="audio/*" capture />
-        </p>
-        <p>
-          Capture Photo: <input type="file" capture="user" accept="image/*" />
-        </p>
+
         <AudioRecorder setMedia={setMedia} />
         <VideoRecorder setMedia={setMedia} />
+        <ImageRecorder setMedia={setMedia} />
         {media?.type?.includes('audio') ? (
           <audio src={media.data} controls />
         ) : media?.type?.includes('video') ? (
           <video src={media.data} controls />
+        ) : media?.type?.includes('image') ? (
+          <img src={media.data} alt="test" />
         ) : (
           <div>nothing yet</div>
         )}
