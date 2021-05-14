8fccb9af1e47c3fecc157e578a68e4a07472807e
Maximilian Hänsel <mh@maxhaensel.de>
2021-05-14 14:27:40 +0200

    rename moments

--- frontend/src/components/card/moments/imageCard.tsx
+++
@@ -1,9 +0,0 @@
-import { IonItem, IonThumbnail, IonImg } from '@ionic/react'
-
-export interface ImageVariant {
-  variant: 'image'
-  image: string
-}
-export const ImageVariantCard: React.FC<ImageVariant> = props => (
-  <IonImg src="http://placekitten.com/g/400/200" />
-)
