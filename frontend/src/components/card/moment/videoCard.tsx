1d36616bd783637c74df3a22d5072f5914136717
Maximilian Hänsel <mh@maxhaensel.de>
2021-05-16 13:53:10 +0200

    remove not required code
    Co-authored-by: Philipp Schmid <32632186+philschmid@users.noreply.github.com>

--- frontend/src/components/card/moment/videoCard.tsx
+++ frontend/src/components/card/moment/videoCard.tsx
@@ -8,7 +8,6 @@ export const VideoVariantCard: React.FC<VideoVariant> = props => (
       src="https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4"
       type="video/mp4"
     />
-    {/* <source src="movie.ogg" type="video/ogg" /> */}
     Your browser does not support the video tag.
   </video>
 )
