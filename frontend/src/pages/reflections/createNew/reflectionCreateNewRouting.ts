f48b4260a96741d81edea4be17644b84c86da431
Maximilian Hänsel <mh@maxhaensel.de>
2021-06-08 18:03:05 +0200

    create static properties

---
+++ frontend/src/pages/reflections/createNew/reflectionCreateNewRouting.ts
@@ -0,0 +1,6 @@
+export enum ReflectionsRouting {
+  'module' = '/reflections/module',
+  'selectTopic' = `/reflections/module/select-topic`,
+  'selectMoments' = `/reflections/module/select-moments`,
+  'writeReport' = `/reflections/module/write-report`,
+}
