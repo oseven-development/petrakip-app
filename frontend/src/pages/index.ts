c124f00d9f402b3ecd57a62fa6afeef2e8b4aee5
Maximilian Hänsel <mh@maxhaensel.de>
2021-06-08 18:03:43 +0200

    create and editviews

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -10,6 +10,10 @@ export { ProfileChangePasswordView } from './profile/profileChangePasswordView'
 export { ProgressDetailView } from './progress/progressDetailView'
 
 /* Reflections */
+export { ReflectionsRouting } from './reflections/createNew/reflectionCreateNewRouting'
 export { ReflectionsListView } from './reflections/reflectionsListView'
 export { ReflectionsDetailView } from './reflections/reflectionsDetailView'
-export { ReflectionsCreateNewView } from './reflections/reflectionsCreateNewView'
+export { ReflectionsCreateNewView } from './reflections/createNew/reflectionsCreateNewView'
+export { ReflectionSelectMomentsView } from './reflections/createNew/reflectionSelectMomentsView'
+export { ReflectionSelectTopicAreaView } from './reflections/createNew/reflectionSelectTopicAreaView'
+export { ReflectionWriteReportView } from './reflections/createNew/reflectionWriteReportView'
---
+++ frontend/src/pages/reflections/createNew/reflectionSelectMomentsView.tsx
@@ -0,0 +1,96 @@
+import React from 'react'
+import { IonButton, IonContent, IonItem, IonPage, IonText } from '@ionic/react'
+import { Header } from '../../../components'
+import { RouteComponentProps, useLocation } from 'react-router'
+
+import { ReflectionQueryParamKeys } from './reflectionQueryParamKeys'
+import { ReflectionsRouting } from './reflectionCreateNewRouting'
+
+import { loadallmoments } from '../../../api/reflection/createReflection'
+import { Moment } from '../../../API'
+import { useUpdateQueryParamState } from './useUpdateQueryParamState'
+
+interface Props extends RouteComponentProps<{}> {}
+
+interface MomentWithSelected extends Moment {
+  selected: boolean
+}
+
+export const ReflectionSelectMomentsView: React.FC<Props> = ({ history }) => {
+  const location = useLocation()
+  const [state, setState] = React.useState<{
+    [key: string]: MomentWithSelected
+  }>({})
+
+  const { currentUrl, UpdateURL } = useUpdateQueryParamState(history)
+
+  React.useEffect(() => {
+    const state: { [key: string]: MomentWithSelected } = {}
+
+    const params = new URLSearchParams(location.search)
+    const array = params.get(ReflectionQueryParamKeys.moment)?.split(',')
+
+    loadallmoments().then(moments => {
+      moments.forEach(moment => {
+        if (moment.id) {
+          const preSelected = array?.includes(moment.id)
+          state[moment.id] = { ...moment, selected: preSelected || false }
+        }
+      })
+      setState(state)
+    })
+  }, [setState, location])
+
+  const setMyState = (key: string) => {
+    setState(state => {
+      const item = state[key]
+      return {
+        ...state,
+        [key]: { ...item, selected: !state[key].selected },
+      }
+    })
+  }
+
+  React.useEffect(() => {
+    const selectedMoments = Object.entries(state)
+      .filter(([key, value]) => value.selected)
+      .map(([key]) => key)
+      .toString()
+
+    UpdateURL(ReflectionQueryParamKeys.moment, selectedMoments)
+  }, [UpdateURL, state])
+
+  return (
+    <IonPage>
+      <Header>Select Moment</Header>
+      <IonContent fullscreen>
+        {Object.entries(state).map(([key, value]) => {
+          return (
+            <IonItem
+              color={value.selected ? 'primary' : undefined}
+              key={key}
+              id={key}
+              onClick={() => {
+                setMyState(key)
+              }}
+            >
+              <IonText>
+                {value.title}
+                <h6 style={{ fontSize: '0.5em' }}>{key}</h6>
+              </IonText>
+            </IonItem>
+          )
+        })}
+        <br></br>
+
+        <IonButton
+          onClick={() => {
+            history.push(`${ReflectionsRouting.module}${currentUrl}`)
+          }}
+        >
+          Fertig
+        </IonButton>
+      </IonContent>
+    </IonPage>
+  )
+}
