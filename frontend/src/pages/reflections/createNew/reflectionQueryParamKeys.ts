6c4b9d097daceafe1f7f9d18edd544c735e6b290
Maximilian Hänsel <mh@maxhaensel.de>
2021-06-08 18:03:05 +0200

    create static properties

---
+++ frontend/src/pages/reflections/createNew/reflectionQueryParamKeys.ts
@@ -0,0 +1,9 @@
+export enum ReflectionQueryParamKeys {
+  'id' = 'id',
+  'topic' = 'topic',
+  'subTopic' = 'sub-topic',
+  'title' = 'title',
+  'moment' = 'moment-ids',
+  'report' = 'report',
+  'createdAt' = 'createdAt',
+}
