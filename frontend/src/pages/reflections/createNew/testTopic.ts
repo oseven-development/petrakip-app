d3c181db2c5971d1294a094091aecda17b3f1283
Maximilian Hänsel <mh@maxhaensel.de>
2021-06-08 18:02:25 +0200

    add test-topics

---
+++ frontend/src/pages/reflections/createNew/testTopic.ts
@@ -0,0 +1,293 @@
+const array = [
+  {
+    topicItemLable: '1 Fachinhalte',
+    topicItemId: '1-fachinhalte',
+    topicItemDescribe:
+      'Die Lehrperson verfügt über fachwissenschaftliches und fachdidaktisches Wissen, versteht die Inhalte, Strukturen und zentralen Forschungsmethoden ihrer Fachbereiche und sie kann Lernsituationen schaffen, die die fachwissenschaftlichen und fachdidaktischen Aspekte für die Lernenden bedeutsam machen.',
+    subListItems: [
+      {
+        subTopicItemId: '1.1-fachwissen-und-didaktik',
+        subjectLable: '1.1 Fachwissen und Didaktik',
+        subjectStatusCompleted: false,
+      },
+      {
+        subTopicItemId: '1.2-forschungsmethoden',
+        subjectLable: '1.2 Forschungsmethoden',
+        subjectStatusCompleted: false,
+      },
+      {
+        subTopicItemId: '1.3-zusammenhänge-erkennen',
+        subjectLable: '1.3 Zusammenhänge erkennen',
+        subjectStatusCompleted: false,
+      },
+      {
+        subTopicItemId: '1.4-auswahl-von-zielen-und-inhalten',
+        subjectLable: '1.4 Auswahl von Zielen und Inhalten',
+        subjectStatusCompleted: true,
+      },
+    ],
+  },
+  {
+    topicItemLable: '2 Entwicklungsprozesse',
+    topicItemId: '2-entwicklungsprozesse',
+    topicItemDescribe:
+      'Die Lehrperson versteht, wie Kinder und Erwachsene lernen und sich entwickeln, und sie kann Lerngelegenheiten und Lernwege anbieten, welche die kognitive, soziale und persönliche Entwicklung unterstützen.',
+    subListItems: [
+      {
+        subTopicItemId: '2.1-entwicklung-der-lernenden',
+        subjectLable: '2.1 Entwicklung der Lernenden',
+        subjectStatusCompleted: true,
+      },
+      {
+        subTopicItemId: '2.2-erfahrungen-und-vorwissen',
+        subjectLable: '2.2 Erfahrungen und Vorwissen',
+        subjectStatusCompleted: true,
+      },
+      {
+        subTopicItemId: '2.3-entwicklungsförderung',
+        subjectLable: '2.3 Entwicklungsförderung',
+        subjectStatusCompleted: false,
+      },
+      {
+        subTopicItemId: '2.4-vielfalt-und-verschiedenheit',
+        subjectLable: '2.4 Vielfalt und Verschiedenheit',
+        subjectStatusCompleted: true,
+      },
+      {
+        subTopicItemId: '2.5-selbstgesteuertes-lernen',
+        subjectLable: '2.5 Selbstgesteuertes Lernen',
+        subjectStatusCompleted: false,
+      },
+    ],
+  },
+  {
+    topicItemLable: '3 Heterogenität beim Lernen',
+    topicItemId: '3-heterogenität-beim-lernen',
+    topicItemDescribe:
+      'Die Lehrperson versteht, wie verschieden die Wege zum Lernen sind, und schafft Unterrichtssituationen, die auf die Lernenden individuell angepasst sind.',
+    subListItems: [
+      {
+        subTopicItemId: '3.1-berücksichtigung-der-lernfähigkeiten',
+        subjectLable: '3.1 Berücksichtigung der Lernfähigkeiten',
+        subjectStatusCompleted: true,
+      },
+      {
+        subTopicItemId: '3.2-zielkriterien',
+        subjectLable: '3.2 Zielkriterien',
+        subjectStatusCompleted: true,
+      },
+      {
+        subTopicItemId: '3.3-leistungserwartungen',
+        subjectLable: '3.3 Leistungserwartungen',
+        subjectStatusCompleted: false,
+      },
+      {
+        subTopicItemId: '3.4-eigenständiges-lernen',
+        subjectLable: '3.4 Eigenständiges Lernen',
+        subjectStatusCompleted: false,
+      },
+    ],
+  },
+  {
+    topicItemLable: '4 Unterrichtsstrategien',
+    topicItemId: '4-unterrichtsstrategien',
+    topicItemDescribe:
+      'Die Lehrperson versteht und verwendet eine Vielfalt von Unterrichtsstrategien, um bei den Lernenden stufengerechte, instrumentelle Fähigkeiten zu entwickeln und zu fördern.',
+    subListItems: [
+      {
+        subTopicItemId: '4.1-aufträge-planen-und-formulieren',
+        subjectLable: '4.1 Aufträge planen und formulieren',
+        subjectStatusCompleted: true,
+      },
+      {
+        subTopicItemId: '4.2-methodenvielfalt',
+        subjectLable: '4.2 Methodenvielfalt',
+        subjectStatusCompleted: true,
+      },
+      {
+        subTopicItemId: '4.3-einsatz-von-lernmaterialien',
+        subjectLable: '4.3 Einsatz von Lernmaterialien',
+        subjectStatusCompleted: true,
+      },
+      {
+        subTopicItemId: '4.4-instrumentelle-fähigkeiten',
+        subjectLable: '4.4 Instrumentelle Fähigkeiten',
+        subjectStatusCompleted: true,
+      },
+    ],
+  },
+  {
+    topicItemLable: '5 Motivieren und Anleiten',
+    topicItemId: '5-motivieren-und-anleiten',
+    topicItemDescribe:
+      'Die Lehrperson setzt ihr Verständnis über Motivationsprozesse und Klassenmanagement gezielt ein, um Lernsituationen zu schaffen, die die positive soziale Zusammenarbeit der Kinder und Jugendlichen fördert und selbstgesteuertes Lernen zulassen.',
+    subListItems: [
+      {
+        subTopicItemId: '5.1-motivationsförderung',
+        subjectLable: '5.1 Motivationsförderung',
+        subjectStatusCompleted: true,
+      },
+      {
+        subTopicItemId: '5.2-unterrichtsfluss',
+        subjectLable: '5.2 Unterrichtsfluss',
+        subjectStatusCompleted: false,
+      },
+      {
+        subTopicItemId: '5.3-zeitmanagement',
+        subjectLable: '5.3 Zeitmanagement',
+        subjectStatusCompleted: false,
+      },
+      {
+        subTopicItemId: '5.4-verhalten-und-klassenklima',
+        subjectLable: '5.4 Verhalten und Klassenklima',
+        subjectStatusCompleted: true,
+      },
+      {
+        subTopicItemId: '5.5-soziale-zusammenarbeit',
+        subjectLable: '5.5 Soziale Zusammenarbeit',
+        subjectStatusCompleted: true,
+      },
+    ],
+  },
+  {
+    topicItemLable: '6 Kommunikation und Moderation',
+    topicItemId: '6-kommunikation-und-moderation',
+    topicItemDescribe:
+      'Die Lehrperson verwendet ihr Wissen von effektiven verbalen und nicht verbalen Kommunikations und Medienformen, um aktives Lernen, Mitarbeit und den gegenseitigen Austausch im Klassenzimmer zu fördern.',
+    subListItems: [
+      {
+        subTopicItemId: '6.1-kommunikationsformen',
+        subjectLable: '6.1 Kommunikationsformen',
+        subjectStatusCompleted: false,
+      },
+      {
+        subTopicItemId: '6.2-moderation',
+        subjectLable: '6.2 Moderation',
+        subjectStatusCompleted: true,
+      },
+      {
+        subTopicItemId: '6.3-diskussionskultur',
+        subjectLable: '6.3 Diskussionskultur',
+        subjectStatusCompleted: false,
+      },
+      {
+        subTopicItemId: '6.4-medieneinsatz',
+        subjectLable: '6.4 Medieneinsatz',
+        subjectStatusCompleted: true,
+      },
+    ],
+  },
+  {
+    topicItemLable: '7 Planen und Evaluieren',
+    topicItemId: '7-planen-und-evaluieren',
+    topicItemDescribe:
+      'Die Lehrperson plant, realisiert und evaluiert ihren Unterricht auf Grund ihres Verständnisses vom Fachbereich, von Lehrplan und Leitideen der Schule, und auf der Basis des berufswissenschaftlichen Hintergrundes.',
+    subListItems: [
+      {
+        subTopicItemId: '7.1-leitideen-und-lehrplan',
+        subjectLable: '7.1 Leitideen und Lehrplan',
+        subjectStatusCompleted: false,
+      },
+      {
+        subTopicItemId: '7.2-systematische-planung',
+        subjectLable: '7.2 Systematische Planung',
+        subjectStatusCompleted: false,
+      },
+      {
+        subTopicItemId: '7.3-situative-anpassung',
+        subjectLable: '7.3 Situative Anpassung',
+        subjectStatusCompleted: false,
+      },
+      {
+        subTopicItemId: '7.4-evaluation',
+        subjectLable: '7.4 Evaluation',
+        subjectStatusCompleted: true,
+      },
+    ],
+  },
+  {
+    topicItemLable: '8 Beobachtung und Förderung',
+    topicItemId: '8-beobachtung-und-förderung',
+    topicItemDescribe:
+      'Die Lehrperson versteht und verwendet gezielt unterschiedliche Beurteilungssysteme, um die kognitive, soziale und persönliche Entwicklung der Kinder und Jugendlichen fortlaufend einzuschätzen, zu sichern und zu fördern.',
+    subListItems: [
+      {
+        subTopicItemId: '8.1-differenzierte-beurteilung',
+        subjectLable: '8.1 Differenzierte Beurteilung',
+        subjectStatusCompleted: true,
+      },
+      {
+        subTopicItemId: '8.2-fördernde-beurteilung',
+        subjectLable: '8.2 Fördernde Beurteilung',
+        subjectStatusCompleted: true,
+      },
+      {
+        subTopicItemId: '8.3-beurteilungskriterien',
+        subjectLable: '8.3 Beurteilungskriterien',
+        subjectStatusCompleted: false,
+      },
+      {
+        subTopicItemId: '8.4-dokumentation-und-kommunikation',
+        subjectLable: '8.4 Dokumentation und Kommunikation',
+        subjectStatusCompleted: true,
+      },
+    ],
+  },
+  {
+    topicItemLable: '9 Reflexion',
+    topicItemId: '9-reflexion',
+    topicItemDescribe:
+      'Die Lehrperson reflektiert fortlaufend die Wirkung ihrer Entscheide und Tätigkeiten auf andere (Lernende, Eltern und auf andere Lehrende) und sie geht ihre professionelle Weiterentwicklung aktiv und verantwortungsbewusst an.',
+    subListItems: [
+      {
+        subTopicItemId: '9.1-unterrichtsreflexion',
+        subjectLable: '9.1 Unterrichtsreflexion',
+        subjectStatusCompleted: true,
+      },
+      {
+        subTopicItemId: '9.2-fremdeinschätzung',
+        subjectLable: '9.2 Fremdeinschätzung',
+        subjectStatusCompleted: false,
+      },
+      {
+        subTopicItemId: '9.3-unterrichtsteam',
+        subjectLable: '9.3 Unterrichtsteam',
+        subjectStatusCompleted: false,
+      },
+      {
+        subTopicItemId: '9.4-weiterbildung',
+        subjectLable: '9.4 Weiterbildung',
+        subjectStatusCompleted: false,
+      },
+    ],
+  },
+  {
+    topicItemLable: '10 Kooperation Umfeld',
+    topicItemId: '10-kooperation-umfeld',
+    topicItemDescribe:
+      'Die Lehrperson verhält sich professionell im Umgang mit dem schulischen und gesellschaftlichen Umfeld, pﬂegt konstruktive Beziehungen mit Kolleginnen und Kollegen, Fachstellen, Eltern und Behörden, um ein förderliches Lernklima zu schaﬀen.',
+    subListItems: [
+      {
+        subTopicItemId: '10.1-professionelles-verhalten',
+        subjectLable: '10.1 Professionelles Verhalten',
+        subjectStatusCompleted: false,
+      },
+      {
+        subTopicItemId: '10.2-kontakt-mit-eltern',
+        subjectLable: '10.2 Kontakt mit Eltern',
+        subjectStatusCompleted: false,
+      },
+      {
+        subTopicItemId: '10.3-fachstellen-und-behörden',
+        subjectLable: '10.3 Fachstellen und Behörden',
+        subjectStatusCompleted: true,
+      },
+      {
+        subTopicItemId: '10.4-beteiligung-in-der-schule',
+        subjectLable: '10.4 Beteiligung in der Schule',
+        subjectStatusCompleted: true,
+      },
+    ],
+  },
+]
+export default array
