4c164e0b43a924d47b148c4ccfb2bb58b319ac9b
Maximilian Hänsel <mh@maxhaensel.de>
2021-07-07 13:57:43 +0200

    Feature/check if user exist or invite (#53)
    * create basic function
    
    * return correct struct
    
    * email template
    
    * create basic function
    
    * return correct struct
    
    * email template
    
    * update function
    
    * create basic function
    
    * return correct struct
    
    * email template
    
    * update function
    
    * added correct cloudformation for invite message
    
    * correct grammar
    
    * Add Todo - TypeError
    
    * update Schema
    
    * update schema amplify generated code
    
    * update Backend
    
    * add prettierignore and not lint autogenerated code
    
    * update gitignore
    
    * Update .gitignore
    
    * update and remove src.egg-info
    
    Co-authored-by: philschmid <schmidphilipp1995@gmail.com>

--- amplify/backend/function/checkUserAndShareAsset/src/index.py
+++ amplify/backend/function/checkUserAndShareAsset/src/index.py
@@ -1,19 +1,143 @@
-import json
 import boto3
+import os
+import logging
+import re
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 # You can access the following resource attributes as environment variables from your Lambda function
 #         AUTH_PETRAKIPAUTH_USERPOOLID
 #         ENV
 #         REGION
-def handler(event, context):
-    print("received event:")
-    print(event)
 
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Access-Control-Allow-Headers": "*",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
-        },
-        "body": json.dumps("Hello from your new Amplify Python lambda!"),
-    }
+session = boto3.session.Session(region_name="eu-central-1")
+client = session.client("cognito-idp")
+
+UserPoolId = os.environ["AUTH_PETRAKIPAUTH_USERPOOLID"]
+
+
+def createUser(email_address):
+    # Returntype
+    # {
+    #     "User": {
+    #         "Username": "user3@maxhaensel.de",
+    #         "Attributes": [
+    #             {"Name": "sub", "Value": "081fd72d-2478-40de-9022-540e2cd6f90f"},
+    #             {"Name": "email_verified", "Value": "true"},
+    #             {"Name": "email", "Value": "user3@maxhaensel.de"},
+    #         ],
+    #         "UserCreateDate": datetime.datetime(2021, 6, 16, 20, 49, 27, 131000, tzinfo=tzlocal()),
+    #         "UserLastModifiedDate": datetime.datetime(2021, 6, 16, 20, 49, 27, 131000, tzinfo=tzlocal()),
+    #         "Enabled": True,
+    #         "UserStatus": "FORCE_CHANGE_PASSWORD",
+    #     },
+    #     "ResponseMetadata": {
+    #         "RequestId": "310b4d9a-fefa-439a-88af-18770497d6e5",
+    #         "HTTPStatusCode": 200,
+    #         "HTTPHeaders": {
+    #             "date": "Wed, 16 Jun 2021 18:49:27 GMT",
+    #             "content-type": "application/x-amz-json-1.1",
+    #             "content-length": "334",
+    #             "connection": "keep-alive",
+    #             "x-amzn-requestid": "310b4d9a-fefa-439a-88af-18770497d6e5",
+    #         },
+    #         "RetryAttempts": 0,
+    #     },
+    # }
+    try:
+        # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cognito-idp.html#CognitoIdentityProvider.Client.admin_create_user
+        response = client.admin_create_user(
+            UserPoolId=UserPoolId,
+            Username=email_address,
+            TemporaryPassword="password",
+            UserAttributes=[
+                {"Name": "email", "Value": email_address},
+                {"Name": "email_verified", "Value": "true"},
+            ],
+            MessageAction="SUPPRESS",
+        )
+
+    except Exception as error:
+        logger.error(error)
+        raise Exception(f"unable to create User with email {email_address}")
+
+    try:
+        for attributes in response["User"]["Attributes"]:
+            if attributes["Name"] == "sub":
+                uuid = attributes["Value"]
+            if attributes["Name"] == "email":
+                email = attributes["Value"]
+        return {"uuid": uuid, "email": email, "alreadyExist": False}
+    except Exception as error:
+        logger.error(error)
+        raise error
+
+
+def gather_user(email_address):
+    try:
+        # Returntype
+        # {
+        #     "Username": "user3@maxhaensel.de",
+        #     "UserAttributes": [
+        #         {"Name": "sub", "Value": "081fd72d-2478-40de-9022-540e2cd6f90f"},
+        #         {"Name": "email_verified", "Value": "true"},
+        #         {"Name": "email", "Value": "user3@maxhaensel.de"},
+        #     ],
+        #     "UserCreateDate": datetime.datetime(2021, 6, 16, 20, 49, 27, 131000, tzinfo=tzlocal()),
+        #     "UserLastModifiedDate": datetime.datetime(2021, 6, 16, 20, 49, 27, 131000, tzinfo=tzlocal()),
+        #     "Enabled": True,
+        #     "UserStatus": "FORCE_CHANGE_PASSWORD",
+        #     "ResponseMetadata": {
+        #         "RequestId": "e3dd98a4-b3d4-430a-843a-e3c3daff34ce",
+        #         "HTTPStatusCode": 200,
+        #         "HTTPHeaders": {
+        #             "date": "Wed, 16 Jun 2021 19:05:41 GMT",
+        #             "content-type": "application/x-amz-json-1.1",
+        #             "content-length": "329",
+        #             "connection": "keep-alive",
+        #             "x-amzn-requestid": "e3dd98a4-b3d4-430a-843a-e3c3daff34ce",
+        #         },
+        #         "RetryAttempts": 0,
+        #     },
+        # }
+
+        response = client.admin_get_user(UserPoolId=UserPoolId, Username=email_address)
+        logger.info(response)
+        for attributes in response["UserAttributes"]:
+            if attributes["Name"] == "sub":
+                uuid = attributes["Value"]
+            if attributes["Name"] == "email":
+                email = attributes["Value"]
+        return {"uuid": uuid, "email": email, "alreadyExist": True}
+
+    except client.exceptions.UserNotFoundException as error:
+        logger.info(f"create new user with email: {email_address}")
+        return createUser(email_address)
+    except Exception as error:
+        logger.error(error)
+        raise error
+
+
+def handler(event, context):
+    logger.info(event)
+    try:
+        username = event["arguments"]["username"]
+    except Exception as error:
+        logger.error(error)
+        raise Exception(f"unable to extract username")
+
+    # https://stackoverflow.com/questions/201323/how-to-validate-an-email-address-using-a-regular-expression
+    # Check if is it a regular mail!
+    try:
+        result = re.match("[^@]+@[^@]+\.[^@]+", username)
+        if not result:
+            raise Exception(f"username {username} is not a regular email-adress")
+    except Exception as error:
+        logger.error(error)
+        raise Exception(f"username {username} is not a regular email-adress")
+
+    logger.info(username)
+    response = gather_user(username)
+
+    return response
--- amplify/backend/function/createSignedUrlForAssets/src/index.py
+++ amplify/backend/function/createSignedUrlForAssets/src/index.py
@@ -1,16 +1,19 @@
+import json
 import boto3
-
+import os
 
 def handler(event, context):
     try:
-        client = boto3.client("s3")
+        client = boto3.client('s3')
         body = event["arguments"]
-
+        
         client.head_object(Bucket=body["bucket"], Key=body["key"])
 
         url = client.generate_presigned_url(
-            ClientMethod="get_object", Params={"Bucket": body["bucket"], "Key": body["key"]}, ExpiresIn=300
-        )
+        ClientMethod='get_object', 
+        Params={'Bucket': body["bucket"], 'Key': body["key"]},
+        ExpiresIn=300)
         return url
     except Exception as e:
         raise e
+        
