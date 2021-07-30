ba00764ba22f10c43d29cc2d02fa470b7552af30
Maximilian Hänsel <mh@maxhaensel.de>
2021-07-30 22:59:09 +0200

    export functions

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,12 +1,12 @@
-export {
-  saveReflectionAPI,
-  loadAllMomentsAPI,
-} from './reflection/saveReflection'
+export { saveReflectionAPI } from './reflection/saveReflection'
 
 export {
   listAllReflectionsAPI,
   listAllReflectionsTopicsAPI,
 } from './reflection/listAllReflections'
+
+export { delteReflectionAPI } from './reflection/deleteReflection'
+
 export { getReflectionAPI } from './reflection/getReflection'
 
 export { shareAPI } from './share/share'
