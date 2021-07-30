6460a0ab9bfa8d348fc474842fbade57f67fd752
Maximilian Hänsel <mh@maxhaensel.de>
2021-07-30 22:58:59 +0200

    refactor moment api

--- frontend/src/api/moment/getMoment.ts
+++ frontend/src/api/moment/getMoment.ts
@@ -1,21 +1,27 @@
 import { API, graphqlOperation } from 'aws-amplify'
 import { getMoment, listMoments } from '../../graphql/queries'
+import { Moment } from '../../API'
 
 export interface getMomentAPIInput {
   id?: string
 }
 
-export const getMomentAPI = async ({ id }: getMomentAPIInput) => {
+async function getMomentAPI({ id }: getMomentAPIInput): Promise<Moment>
+async function getMomentAPI(): Promise<Moment[]>
+async function getMomentAPI(input?: getMomentAPIInput) {
   try {
-    if (id) {
+    if (input?.id) {
+      const { id } = input
       const result: any = await API.graphql(graphqlOperation(getMoment, { id }))
-      return result.data.getMoment
+      return result.data.getMoment as Moment
     } else {
       const result: any = await API.graphql(graphqlOperation(listMoments))
-      return result.data.listMoments
+      return result.data.listMoments.items as Moment[]
     }
   } catch (error) {
     console.error(error)
     throw error
   }
 }
+
+export { getMomentAPI }
