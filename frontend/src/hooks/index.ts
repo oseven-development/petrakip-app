8490cbc499740f94e91ac14f24cc4ddea1258bb2
Maximilian Hänsel <mh@maxhaensel.de>
2021-09-06 21:53:39 +0200

    create useDebounce hook

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useCustomLoaderOnViewEnter } from './useCustomLoaderOnViewEnter'
 export { useCustomLoaderOnTrigger } from './useCustomLoaderOnTrigger'
 export { usePlatform } from './usePlatform'
+export { useDebounce } from './useDebounce'
